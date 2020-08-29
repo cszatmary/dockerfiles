0ecb4f8fa51e53f00cd2760d688cfd2b82391eb5
Christopher Szatmary <cs@christopherszatmary.com>
2020-08-29 18:39:57 -0400

    Configure CI to automatically push images to dockerhub

---
+++ .circleci/image_names.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script finds all the images names that were modified in the last commit
+# It excludes blacklisted items so we don't try building and pushing images for those
+
+BLACKLIST=".circleci README.md"
+NAMES=$(git diff --name-only HEAD^ HEAD | cut -d '/' -f 1)
+for NAME in $NAMES; do
+    if ! $(echo "$BLACKLIST" | grep -F -q -w "$NAME"); then
+        echo "$NAME"
+    fi
+done
