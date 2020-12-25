b8cc0d61912903c6f8c6561a132e3ac865d55e8b
Christopher Szatmary <cs@christopherszatmary.com>
2020-12-25 18:49:57 -0500

    Switch to new account, clean up images (#15)
    * Delete unneeded images
    
    * Switch to new account, cleanup images

--- .circleci/image_names.sh
+++ .circleci/image_names.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # This script finds all the images names that were modified in the last commit
-# It excludes blacklisted items so we don't try building and pushing images for those
+# It excludes non-dockerfile items so we don't try building and pushing images for those
 
-BLACKLIST=".circleci README.md"
-NAMES=$(git diff --name-only HEAD^ HEAD | cut -d '/' -f 1)
+EXCLUDE=".circleci README.md"
+NAMES=$(git diff --name-only --diff-filter=ACMR HEAD^ HEAD | cut -d '/' -f 1)
 for NAME in $NAMES; do
-    # Make sure it's not in the blacklist and it exists
-    if ! echo "$BLACKLIST" | grep -F -q -w "$NAME" && [[ -d "$NAME" ]]; then
+    # Make sure it's not in the exclude list and it exists
+    if ! echo "$EXCLUDE" | grep -F -q -w "$NAME"; then
         echo "$NAME"
     fi
 done
